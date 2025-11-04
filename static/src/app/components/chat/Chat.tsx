7ae9de5656a995896d9736e5d741f75229ebde97
Markus <abramianmedina@gmail.com>
2025-11-04 00:36:25 +0000

    logo added

--- static/src/app/components/chat/Chat.tsx
+++ static/src/app/components/chat/Chat.tsx
@@ -1,13 +1,22 @@
 "use client";
-import React from "react";
 import Input from "./Input";
 import DatabaseDiagram from "../diagram/DatabaseDiagram";
+import Image from "next/image";
 
 export default function Chat() {
     return (
-        <div className="flex flex-col h-full bg-white">
-            {/* Contenedor del diagrama (ocupa todo menos el input) */}
-            <div className="flex-1">
+        <div className="relative flex flex-col h-full bg-white">
+            <div className="absolute top-1 right-4 z-30">
+                <Image
+                    src="/logo.png"
+                    alt="Logo"
+                    width={80}
+                    height={80}
+                    className="opacity-90 hover:opacity-100 transition-opacity duration-200"
+                />
+            </div>
+
+            <div className="flex-1 overflow-hidden">
                 <DatabaseDiagram />
             </div>
 
