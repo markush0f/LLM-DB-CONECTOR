00bc3bff6293a4c19100629140113b0a6d21945f
Markus <abramianmedina@gmail.com>
2025-11-07 00:02:06 +0000

    Allow cors

--- app/main.py
+++ app/main.py
@@ -4,10 +4,24 @@ from app.services.internal_database_service import DatabaseService
 from app.router.connections_router import router as connections_router
 from app.router.llm_sql_router import router as llm_sql_router
 from app.router.schema_router import router as schema_router
+from fastapi.middleware.cors import CORSMiddleware
+
 
 init_internal_db()
 
 app = FastAPI(title="LLM-DB CONNECTOR API", version="1.0.0")
+origins = [
+    "http://localhost:3000", 
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 db_service = DatabaseService()
 app.include_router(connections_router)
 app.include_router(llm_sql_router)
